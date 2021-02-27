3ec5df1462b957b539f51812469ec971008d1f73
alexadhy <alex.dhyatma@ubuntusoftware.net>
2021-02-27 18:51:32 +0700

    cleanup cache

---
+++ cmd/cache.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"go.amplifyedge.org/booty-v2/dep"
+)
+
+func CleanCacheCmd(o dep.Executor) *cobra.Command {
+	cleanCmd := &cobra.Command{
+		Use:                        "clean",
+		Short:                      "clean",
+		Args:                       cobra.NoArgs,
+		ArgAliases:                 nil,
+		BashCompletionFunction:     "",
+		Deprecated:                 "",
+		Annotations:                nil,
+		Version:                    "",
+		PersistentPreRun:           nil,
+		PersistentPreRunE:          nil,
+		PreRun:                     nil,
+		PreRunE:                    nil,
+		Run:                        nil,
+		RunE:                       nil,
+		PostRun:                    nil,
+		PostRunE:                   nil,
+		PersistentPostRun:          nil,
+		PersistentPostRunE:         nil,
+		FParseErrWhitelist:         cobra.FParseErrWhitelist{},
+		TraverseChildren:           false,
+		Hidden:                     false,
+		SilenceErrors:              false,
+		SilenceUsage:               false,
+		DisableFlagParsing:         false,
+		DisableAutoGenTag:          false,
+		DisableFlagsInUseLine:      false,
+		DisableSuggestions:         false,
+		SuggestionsMinimumDistance: 0,
+	}
+}
